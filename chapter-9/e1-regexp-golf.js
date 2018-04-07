19e563e392a2dc69a3c2be8085d84e4c1f0e4e32
owlnerd <33882315+owlnerd@users.noreply.github.com>
2018-04-07 13:41:20 +0200

    Update e1-regexp-golf.js

--- chapter-9/e1-regexp-golf.js
+++ chapter-9/e1-regexp-golf.js
@@ -14,13 +14,13 @@
   about word boundaries unless explicitly mentioned. When your expression
   works, see whether you can make it any smaller.
 
-  1. 'car' and 'cat'
-  2. 'pop' and 'prop'
-  3. 'ferret', 'ferry', and 'ferrari'
-  4. Any word ending in 'ious'
-  5. A whitespace character followed by a period, comma, colon, or semicolon
-  6. A word longer than six letters
-  7. A word without the letter e (or E)
+    1. 'car' and 'cat'
+    2. 'pop' and 'prop'
+    3. 'ferret', 'ferry', and 'ferrari'
+    4. Any word ending in 'ious'
+    5. A whitespace character followed by a period, comma, colon, or semicolon
+    6. A word longer than six letters
+    7. A word without the letter e (or E)
 
   Refer to the table in the chapter summary for help. Test each solution with a
   few test strings.
